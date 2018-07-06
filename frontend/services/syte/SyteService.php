15849336073aa4e821e8b0e5094c877a639b441e
user <user@kontent>
2018-07-06 17:35:08 +0300

    fix Send mail

--- frontend/services/syte/SyteService.php
+++ frontend/services/syte/SyteService.php
@@ -2,6 +2,7 @@
 
 namespace frontend\services\syte;
 
+use Yii;
 use frontend\models\Contact;
 
 class SyteService
@@ -20,7 +21,12 @@ class SyteService
     public function sendLetter()
     {
         $message = "Tекст: {$this->contact->userText} . от {$this->contact->userEmail}";
-        $result = mail($this->adminEmail, $this->letterTheme, $message);
+        //$result = mail($this->adminEmail, $this->letterTheme, $message);
+        $result = Yii::$app->mailer->compose()
+                ->setFrom($this->contact->userEmail)
+                ->setTo($this->adminEmail)
+                ->setTextBody($message)
+                ->send();
         if (!$result) {
             throw new \RuntimeException("Don't send email");
         }
