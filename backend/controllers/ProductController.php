6f7d588344d91d10c99aa2fb97882cd743f0ca44
user <user@kontent>
2018-08-10 13:15:09 +0300

    add ProductController

---
+++ backend/controllers/ProductController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace backend\controllers;
+
+use backend\models\repository\CategoryRepository;
+use backend\models\repository\ProductRepository;
+use Yii;
+use backend\models\UploadForm;
+use yii\web\UploadedFile;
+
+class ProductController extends \yii\web\Controller
+{
+    private $productRepository;
+    private $categoryRepository;
+
+    public function __construct($id,
+                                $module,
+                                ProductRepository $productRepository,
+                                CategoryRepository $categoryRepository,
+                                array $config = [])
+    {
+        $this->productRepository = $productRepository;
+        $this->categoryRepository = $categoryRepository;
+        parent::__construct($id, $module, $config);
+    }
+
+    public function actionIndex()
+    {
+        $products = $this->productRepository->getAllProducts();
+        return $this->render('index', [
+            'products' => $products,
+        ]);
+    }
+
+    public function actionCreate()
+    {
+        return $this->render('create', [
+
+        ]);
+    }
+
+    public function actionUpdate($id)
+    {
+        $product = $this->productRepository->getProductsById($id);
+        $category = $this->categoryRepository->getCategoryList();
+        $model = new UploadForm();
+
+        if (yii::$app->request->isPost) {
+            $model->image = UploadedFile::getInstance($model, 'image');
+            $model->validate();
+            $formData = yii::$app->request->post();
+        }
+
+        return $this->render('update', [
+            'product' => $product,
+            'category' => $category,
+            'model' => $model,
+        ]);
+    }
+
+    public function actionDelete($id)
+    {
+        return $this->render('delete', [
+
+        ]);
+    }
+}
